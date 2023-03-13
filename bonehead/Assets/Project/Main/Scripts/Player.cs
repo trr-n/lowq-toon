4ae4351b739479eadccb3febff7451604d4fa94f
cet <cets021@gmail.com>
2023-03-13 22:07:41 +0900

    a

--- bonehead/Assets/Project/Main/Scripts/Player.cs
+++ bonehead/Assets/Project/Main/Scripts/Player.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    static GameObject camObj;
-    static Vector3 self;
-    static Rigidbody rb;
+    new GameObject camera;
+    Vector3 cameraPos;
 
-    new Player.Camera camera;
-    Movement move;
+    GameObject self;
+    Vector3 selfPos;
+    static Rigidbody selfRb;
+
+    // Movement movement;
+    // Camera camera;
 
     void Start()
     {
@@ -18,45 +21,48 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        camera.Pos(gap: .5f);
-        // move.Move(addition: 5);
-        // move.Jump(power: 5);
+        // { #0ff }
+        camera.transform.Translate(
+            Camera.Pos(camera.transform.position, gap: .5f));
+
+        // { #fff }
+        self.transform.Translate(Movement.Move(addition: 5));
+        // Movement.Jump(power: 5);
+
     }
 
     void Get()
     {
-        camObj = GameObject.FindGameObjectWithTag(MyApp.Tags.CAM);
-        self = this.gameObject.transform.position;
-        rb = this.gameObject.GetComponent<Rigidbody>();
+        // { #0ff }
+        camera = GameObject.FindGameObjectWithTag(MyApp.Tags.CAM);
+        cameraPos = camera.transform.position;
+
+        // { #fff }
+        selfPos = gameObject.transform.position;
+        self = gameObject;
+        selfRb = gameObject.GetComponent<Rigidbody>();
+
     }
 
-    class Camera : Player
+    class Camera //: Player
     {
-        new GameObject camObj;
-
-        public void Pos(float gap)
-        {
-            Vector3 camPos = camObj.transform.position;
-            camPos = new(self.x, self.y, self.z + gap);
-            transform.Translate(camPos);
-        }
+        public static Vector3 Pos(Vector3 basePos, float gap)
+        => new(basePos.x, basePos.y, basePos.z + gap);
     }
 
-    class Movement : Player
+    class Movement //: MonoBehaviour
     {
-        public void Move(float addition)
+        public static Vector3 Move(float addition)
         {
             float h = Input.GetAxis(MyApp.Keys.HORIZONTAL);
             Vector3 widen = new(h, 0, 0);
-            transform.Translate(widen * addition);
+            return widen * addition;
         }
 
         public void Jump(float power)
         {
             if (Input.GetButton(MyApp.Keys.JUMP))
-            {
-                rb.AddForce(Vector3.up * power, ForceMode.Impulse);
-            }
+                selfRb.AddForce(Vector3.up * power, ForceMode.Impulse);
         }
     }
 }
