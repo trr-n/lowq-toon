55a424dcb7d0597e2f6d63c71f1787a02aa32bfd
cet <cets021@gmail.com>
2023-03-14 13:36:00 +0900

    a

--- bonehead/Assets/Project/Main/Scripts/Player.cs
+++ bonehead/Assets/Project/Main/Scripts/Player.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // {#fff}
+    public class Temp
+    {
+        public static float JumpingPower = 1;
+        public static Vector3 JumpingDirection = Vector3.up;
+        //
+        public static float MovingSpeed = 20f;
+    }
+
+
     new GameObject camera;
     Vector3 cameraPos;
 
@@ -11,8 +21,10 @@ public class Player : MonoBehaviour
     Vector3 selfPos;
     static Rigidbody selfRb;
 
-    // Movement movement;
-    // Camera camera;
+    /*
+    Movement movement;
+    Camera camera;
+    */
 
     void Start()
     {
@@ -21,27 +33,28 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        // { #0ff }
+        print(Temp.JumpingPower);
+
         camera.transform.Translate(
             Camera.Pos(camera.transform.position, gap: .5f));
 
-        // { #fff }
+        // Move
         self.transform.Translate(Movement.Move(addition: 5));
-        // Movement.Jump(power: 5);
+
+        // Jump
+        Movement.Jump(power: 5, direction: Vector3.up);
+        // selfRb.AddForce();
 
     }
 
     void Get()
     {
-        // { #0ff }
         camera = GameObject.FindGameObjectWithTag(MyApp.Tags.CAM);
         cameraPos = camera.transform.position;
 
-        // { #fff }
         selfPos = gameObject.transform.position;
         self = gameObject;
         selfRb = gameObject.GetComponent<Rigidbody>();
-
     }
 
     class Camera //: Player
@@ -59,10 +72,10 @@ public class Player : MonoBehaviour
             return widen * addition;
         }
 
-        public void Jump(float power)
+        public static void Jump(float power, Vector3 direction)
         {
             if (Input.GetButton(MyApp.Keys.JUMP))
-                selfRb.AddForce(Vector3.up * power, ForceMode.Impulse);
+                selfRb.AddForce(direction * power, ForceMode.Impulse);
         }
     }
 }
