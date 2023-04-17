b9214b652f88d4004ca58e1bd09700f3e83cd2fa
s7ez <s7eeez@gmail.com>
2023-04-17 16:41:38 +0900

    2

--- plat/Assets/Project/Main/Scripts/Shoot.cs
+++ plat/Assets/Project/Main/Scripts/Shoot.cs
@@ -6,47 +6,47 @@ public class Shoot : MonoBehaviour
 {
     [SerializeField]
     GameObject bullet;
-
     [SerializeField]
-    float power = 100;
+    GameObject player;
+    [SerializeField]
+    float power;
+
+    /// <summary>
+    /// 銃口
+    /// </summary>
+    Vector3 nozzlePos = new(24.5f, 9, 20);
 
     /// <summary>
     /// 発射レート
     /// </summary>
     [SerializeField]
-    float rapid = 0.5f;
+    float fireRate = 0.5f;
 
     float timer;
 
-    PlayerMovement player;
-
-    void Start()
-    {
-
-    }
-
     void Update()
     {
-        player = GetComponent<PlayerMovement>();
+        Debug.Log(timer);
 
-        print(timer);
-        if (Input.GetMouseButton(0))
+        // if (Input.GetMouseButton(0))
+        if (true)
         {
-            Fire(power);
+            Fire(power: power);
         }
+        var str = ; // 銃口と弾の位置を計算
     }
 
     void Fire(float power)
     {
         timer += Time.deltaTime;
 
-        if (timer < rapid) return;
-
-        print("pass");
-        var bulletIns = Instantiate(bullet, transform.position, Quaternion.identity);
+        if (timer <= fireRate) return;
+        var bulletIns = Instantiate(bullet, transform.parent.position + nozzlePos, Quaternion.identity);
         var bulletRb = bulletIns.GetComponent<Rigidbody>();
-        // bulletRb.AddForce(this.gameObject.transform.forward * power, ForceMode.Impulse);
-        bulletRb.AddForce(player.transform.forward * power, ForceMode.Impulse);
+        bulletRb.AddForce(transform.parent.forward * power, ForceMode.Impulse);
+        // bulletRb.AddForce(player.transform.forward * power, ForceMode.Impulse);
+        // bulletRb.velocity = transform.right * power;
+
         timer = 0;
     }
 }
