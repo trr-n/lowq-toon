2098f2536805a0ca9614ae23c7782729dce70eb5
cet <cets021@gmail.com>
2023-06-22 19:53:31 +0900

    update: プレイヤーがポータルから見えなくなったらUIを非表示に

--- plat/Assets/Scripts/PlayerToPortal.cs
+++ plat/Assets/Scripts/PlayerToPortal.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Toon.Extend;
 
 namespace Toon
 {
@@ -8,37 +9,13 @@ namespace Toon
     {
         [SerializeField]
         Transform targetT;
-
-        bool looking = false;
-        public bool Looking => looking;
-
-        Vector3 direction;
-        float distance;
-
-        void Start()
-        {
-
-        }
-
+        [SerializeField]
+        GameObject setumei;
         void Update()
         {
-            direction = targetT.position - transform.position;
-            distance = Vector3.Distance(targetT.position, transform.position);
-            Ray ray = new(transform.position, direction);
-            Debug.DrawRay(ray.origin, ray.direction, Color.white, Time.deltaTime, true);
-            looking = false;
-            if (Physics.Raycast(ray, out var hit, distance))
-            {
-                if (hit.collider)
-                {
-                    print("hit for " + hit.collider.name);
-                }
-                if (hit.collider.CompareTag(constant.Portal))
-                {
-                    looking = true;
-                    print("looking at portal");
-                }
-            }
+            var ray = new Ray(transform.position, targetT.position - transform.position);
+            var distance = Vector3.Distance(targetT.position, transform.position) + 1;
+            setumei.SetActive(Physics.Raycast(ray, out var hit2, distance) && hit2.collider.CompareTag(constant.Player));
         }
     }
 }
